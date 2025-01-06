cf65fdd946a7e7d482c86348f903225173d36b0d
Sophia <sophia@zwrks.com>
2025-01-06 00:51:15 +0000

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,24 @@
+Config = {}
+
+Config.Elevators = {
+    {
+        location = "Example Elevator 1",
+        floors = {
+            {coords = vector3(-322.30, -775.06, 36.97), label = "Floor 1"},
+            {coords = vector3(-322.49, -775.07, 41.78), label = "Floor 2"},
+            {coords = vector3(-322.47, -775.07, 46.61), label = "Floor 3"},
+            {coords = vector3(-322.29, -774.90, 51.42), label = "Floor 4"},
+            {coords = vector3(-322.31, -774.87, 56.25), label = "Floor 5"},
+        }
+    },
+    {
+        location = "Example Elevator 2",
+        floors = {
+            {coords = vec3(-1201.84, -349.77, 38.29), label = "Floor 1"},
+            {coords = vec3(-1201.81, -349.83, 43.79), label = "Floor 2"},
+            {coords = vec3(-1201.76, -349.92, 49.29), label = "Floor 3"},
+            {coords = vec3(-1201.76, -349.92, 54.79), label = "Floor 4"}, 
+            {coords = vec3(-1201.81, -349.84, 60.28), label = "Floor 5"},
+        }
+    }
+}
