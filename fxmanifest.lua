8184f81c23ec5fc7dbd638449fb867696a918859
Sophia <sophia@zwrks.com>
2025-01-06 00:51:34 +0000

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+-- fxmanifest.lua for Elevator Script
+
+fx_version 'cerulean'
+game 'gta5'
+
+author 'laurelnwk.com'
+description 'Standalone Elevator Script with NativeUI Integration'
+version '1.0.0'
+
+-- Client Scripts
+client_scripts {
+    'elevator.lua'
+}
+
+shared_script 'config.lua'
